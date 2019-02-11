b5536958e1662002c833b9d7311df1d5c468b92b
ruidong <ruidong@ebus131.informatik.uni-leipzig.de>
2019-02-11 18:21:41 +0100

    file upload

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ from flask_uploads import UploadSet, configure_uploads, ARCHIVES, IMAGES, patch_
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
+from flask import g
 
+job_id = None
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I have a dream'
 app.config['UPLOADED_ARCHIVES_DEST'] = os.getcwd() + '/static/data'
@@ -23,7 +25,6 @@ archive_data = UploadSet('archives', ARCHIVES)
 # archive_data.default_dest = app.config['UPLOADED_ARCHIVES_DEST']
 configure_uploads(app, archive_data)
 patch_request_class(app, size=16 * 1024 * 1024)  # set maximum file size, default is 16MB
-
 
 class UploadForm(FlaskForm):
     data = FileField(validators=[FileAllowed(archive_data, u'Archives Only!'), FileRequired(u'Choose a file!')])
@@ -44,16 +45,11 @@ def upload_file():
     return render_template('index.html', form=form, success=success)
 
 
-@app.route('/manage')
+#Manage Files
+@app.route('/manage_files')
 def manage_file():
     files_list = os.listdir(app.config['UPLOADED_ARCHIVES_DEST'])
-    return render_template('manage.html', files_list=files_list)
-
-
-@app.route('/open/<filename>')
-def open_file(filename):
-    file_url = archive_data.url(filename)
-    return render_template('browser.html', file_url=file_url)
+    return render_template('manage_files.html', files_list=files_list)
 
 
 @app.route('/delete/<filename>')
@@ -61,6 +57,39 @@ def delete_file(filename):
     file_path = archive_data.path(filename)
     os.remove(file_path)
     return redirect(url_for('manage_file'))
+
+
+#Manage jobs
+@app.route('/manage_jobs')
+def manage_job():
+    global job_id
+    files_list = os.listdir(app.config['UPLOADED_ARCHIVES_DEST'])
+    return render_template('manage_jobs.html', files_list=files_list, job_id=job_id)
+
+
+#Run jobs
+@app.route('/run/<filename>')
+def train_model(filename):
+    global job_id
+    job_id = filename
+    files_list = os.listdir(app.config['UPLOADED_ARCHIVES_DEST'])
+    return render_template('manage_jobs.html', files_list=files_list, job_id=job_id)
+
+
+#Stop Jobs
+@app.route('/stop/<filename>')
+def stop_train(filename):
+    global job_id
+    job_id = None
+    files_list = os.listdir(app.config['UPLOADED_ARCHIVES_DEST'])
+    return render_template('manage_jobs.html', files_list=files_list, job_id=job_id)
+
+
+#Check Status
+@app.route('/check/<filename>')
+def check_status(filename):
+    files_list = os.listdir(app.config['UPLOADED_ARCHIVES_DEST'])
+    return render_template('manage_jobs.html', files_list=files_list, job_id=job_id)
 
 
 if __name__ == '__main__':
