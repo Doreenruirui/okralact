cafb777185f674550e7d3574fc063ff5b9010b72
ruidong <ruidong@ebus131.informatik.uni-leipzig.de>
2019-02-11 17:44:34 +0100

    file upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-###TODO: File Upload
+### File Upload
 ###TODO: Training Status Query
 ###TODO: Trained Model Request
 ###TODO: Stop Training Manually (optional)
@@ -9,22 +9,24 @@ import time
 import hashlib
 
 from flask import Flask, render_template, redirect, url_for, request
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_uploads import UploadSet, configure_uploads, ARCHIVES, IMAGES, patch_request_class
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I have a dream'
-app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/static'
+app.config['UPLOADED_ARCHIVES_DEST'] = os.getcwd() + '/static/data'
+app.config['DOWNLOAD_MODELS_DEST'] = os.getcwd() + '/static/model'
 
-photos = UploadSet('photos', IMAGES)
-configure_uploads(app, photos)
-patch_request_class(app)  # set maximum file size, default is 16MB
+archive_data = UploadSet('archives', ARCHIVES)
+# archive_data.default_dest = app.config['UPLOADED_ARCHIVES_DEST']
+configure_uploads(app, archive_data)
+patch_request_class(app, size=16 * 1024 * 1024)  # set maximum file size, default is 16MB
 
 
 class UploadForm(FlaskForm):
-    photo = FileField(validators=[FileAllowed(photos, u'Image Only!'), FileRequired(u'Choose a file!')])
+    data = FileField(validators=[FileAllowed(archive_data, u'Archives Only!'), FileRequired(u'Choose a file!')])
     submit = SubmitField(u'Upload')
 
 
@@ -32,9 +34,10 @@ class UploadForm(FlaskForm):
 def upload_file():
     form = UploadForm()
     if form.validate_on_submit():
-        for filename in request.files.getlist('photo'):
-            name = hashlib.md5('admin' + str(time.time())).hexdigest()[:15]
-            photos.save(filename, name=name + '.')
+        for filename in request.files.getlist('data'):
+            name = hashlib.md5(('admin' + str(time.time())).encode('utf-8')).hexdigest()[:15]
+            archive_data.save(filename, name=name + '.')
+            print(name)
         success = True
     else:
         success = False
@@ -43,19 +46,19 @@ def upload_file():
 
 @app.route('/manage')
 def manage_file():
-    files_list = os.listdir(app.config['UPLOADED_PHOTOS_DEST'])
+    files_list = os.listdir(app.config['UPLOADED_ARCHIVES_DEST'])
     return render_template('manage.html', files_list=files_list)
 
 
 @app.route('/open/<filename>')
 def open_file(filename):
-    file_url = photos.url(filename)
+    file_url = archive_data.url(filename)
     return render_template('browser.html', file_url=file_url)
 
 
 @app.route('/delete/<filename>')
 def delete_file(filename):
-    file_path = photos.path(filename)
+    file_path = archive_data.path(filename)
     os.remove(file_path)
     return redirect(url_for('manage_file'))
 
