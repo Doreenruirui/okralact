eb02360bf0b4286dc9d141a34a7a47c0163f59a5
ruidong <ruidong@ebus131.informatik.uni-leipzig.de>
2019-08-19 12:53:57 +0200

    better validation

--- engines/validate_parameters.py
+++ engines/validate_parameters.py
@@ -16,28 +16,27 @@ def validate(schema, config):
     error_message = []
     for eno, error in enumerate(validator.iter_errors(config)):
         if len(error.path) > 0:
-            error_message.append('error %d:\tparameter %s, %s' % (eno,  error.path[0], error.message))
+            error_message.append('parameter %s, %s' % (error.path[0], error.message))
         else:
-            error_message.append('error %d:\t%s' % (eno, error.message))
+            error_message.append('%s' % error.message)
     return error_message
 
 
-def validate_model(config):
+def validate_model(model, engine):
     err_str = []
-    engine = config["engine"]
     layers = read_json('engines/schemas/models/layer_all_%s.schema' % engine)["definitions"]
-    for ele in config["model"]:
+    for ele in model:
         for key in ele:
             if key not in layers:
-                err_str.append('Layer %s is not defined in engine %s' % (key, engine))
+                err_str.append('parameter model, layer %s is not defined in the model of engine %s' % (key, engine))
             else:
-                resolver = RefResolver('file://%s/engines/schemas/' % os.getcwd(), None)
-                validator = Draft4Validator(layers[key]["properties"], resolver=resolver)
+                resolver = RefResolver('file://%s/engines/schemas/models/' % os.getcwd(), None)
+                validator = Draft4Validator(layers[key], resolver=resolver)
                 for eno, error in enumerate(validator.iter_errors(ele)):
                     if len(error.path) > 0:
-                        err_str.append('error %d:\tparameter %s, %s' % (eno, error.path[0], error.message))
+                        err_str.append('parameter model, layer %s, %s' % (key, error.message))
                     else:
-                        err_str.append('error %d:\t%s' % (eno, error.message))
+                        err_str.append('parameter model, layer %s, %s' % (key, error.message))
     return err_str
 
 
@@ -52,8 +51,16 @@ def validate_file(config_file):
         engine = config["engine"]
         # print('engines/schemas/%s.schema' % engine)
         engine_schema = read_json('engines/schemas/engine_%s.schema' % engine)
+        errors_model = []
+        if "model" in config:
+            model = config["model"]
+            del config["model"]
+            errors_model = validate_model(model, engine)
         errors = validate(engine_schema, config)
-        errors_model = validate_model(config)
+        errors += errors_model
+        nerr = len(errors)
+        for i in range(nerr):
+            errors[i] = 'Error %d: %s' % (i, errors[i])
         return errors
 
 
